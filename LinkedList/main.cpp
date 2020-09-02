04dd136b7a38e7f3ce1502c9b098d4c487c36861
Ben <doc.ben.johnson@gmail.com>
2020-09-02 11:19:24 -0400

    steps

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,3 +1,14 @@
+// Step 1:  Write the .h public methods
+// Step 2:  Write tests of the public methods
+// Step 3:  Write the .h private methods/fields
+// Step 4:  Write blank method implementations
+// Step 5:  Test
+// Step 6:  Write create operation(s)
+// Step 7:  Test
+// Step 8:  Write retrieve operation(s)
+// Step 9:  Test
+// You get the idea
+
 int main() {
 
 }
